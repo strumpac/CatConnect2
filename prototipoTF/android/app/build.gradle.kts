2577ad68cbd5e6c8c74b167cea1bdbf3df34f6a2
Riccardo <stoppariccardo06@gmail.com>
2025-03-16 14:46:38 +0100

    creato prototipo tensorflow

---
+++ prototipoTF/android/app/build.gradle.kts
@@ -0,0 +1,44 @@
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    id("dev.flutter.flutter-gradle-plugin")
+}
+
+android {
+    namespace = "com.example.cat"
+    compileSdk = flutter.compileSdkVersion
+    ndkVersion = "27.0.12077973"
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_11.toString()
+    }
+
+    defaultConfig {
+        applicationId = "com.example.cat"
+        minSdk = flutter.minSdkVersion
+        targetSdk = flutter.targetSdkVersion
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = true  
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),  
+                file("proguard-rules.pro")
+            )
+            signingConfig = signingConfigs.getByName("debug")
+        }
+    }
+}
+
+
+flutter {
+    source = "../.."
+}
